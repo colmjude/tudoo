7906675ea17683951970a3dd3f2af66218217e31
Colm Britton <colmjude@gmail.com>
2012-09-28 14:48:10 +0100

    Stop :hover effects appearing whening tapping on a touch device
    - test whether there are touch events available
    - if not add a no-touch class to html el

--- src/js/script.js
+++ src/js/script.js
@@ -6,6 +6,12 @@ require([
 	// TODO: fix this, don't want it to be a global
 	ENTER_KEY = 13;
 
+	// check to see if it is on a touch device
+	// if not add a no-touch class to the html el
+	if (!("ontouchstart" in document.documentElement)) {
+		document.documentElement.className += " no-touch";
+	}
+
 	// register function to output right css class in template
 	Handlebars.registerHelper('completed-todo', function() {
 	  return this.completed === true ? 'completed' : '';
